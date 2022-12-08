19504348665323781d167e698d80156bc7a8d87a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-08 08:09:51 -0300

    config: add default colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   tailwindConfig: "./tailwind.config.js",
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        DEFAULT: "#2860BB",
+        dark: "#022898",
+        light: "#6B92C1",
+        lightest: "#9BBDE4",
+      },
+      black: {
+        DEFAULT: "#636363",
+        dark: "#070707",
+        light: "#9E9E9E",
+        lightest: "#E1E1E1",
+      },
   },
   fontFamily: {
     inter: ["Inter", "sans-serif"],
