a8ecb78caf4908a72571600cc31253df68db95df
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-08 07:58:38 -0300

    config: add tailwindcss and prettier

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  tailwindConfig: "./tailwind.config.js",
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    inter: ["Inter", "sans-serif"],
+  },
+  plugins: [],
+};
